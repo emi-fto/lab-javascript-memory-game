eb91328ad213fffafbdb2f4ee5efd0c703ef9996
Emiliano Foti <foti.emiliano@gmail.com>
2023-11-29 19:08:29 +0100

    first iteration done

--- src/memory.js
+++ src/memory.js
@@ -1,15 +1,34 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined
+    }
+    for (let arrIndex = this.cards.length - 1; arrIndex > 0; arrIndex -=1) {
+        const randomIndex = Math.floor(Math.random()*(arrIndex + 1));
+        const temp = this.cards[arrIndex];
+        this.cards[arrIndex] = this.cards[randomIndex];
+        this.cards[randomIndex] = temp;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (this.checkIfPair) {
+      this.pairsClicked +=1;
+    }
+    if (card1.name === card2.name) {
+      this.pairsGuessed +=1
+      return true;
+    } 
+    else {
+      return false;
+    }
   }
 
   checkIfFinished() {
