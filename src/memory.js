897716cd5f71d923268ecb0a893c25f02769da32
Emiliano Foti <foti.emiliano@gmail.com>
2023-11-30 08:53:23 +0100

    Jasmine is happy

--- src/memory.js
+++ src/memory.js
@@ -32,6 +32,10 @@ class MemoryGame {
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length /2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
